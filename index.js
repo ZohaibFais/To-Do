8d67e34890c4046796f6f7977f2d871fab41afa8
ZohaibFais <murtuza.tcc@gmail.com>
2024-03-05 14:36:11 +0500

    complete

--- index.js
+++ index.js
@@ -4,41 +4,41 @@ var listContainer = document.getElementById("list-container")
 function addData(){
     if (inputData.value == ""){
         alert("You haven't entered any task")
+        storeData()
     }
     else{
        var li = document.createElement("li")
        li.innerHTML = inputData.value
-       listContainer.appendChild(li)
-       var span = document.createElement("span")
-       span.innerHTML = "\u00d7"
-       li.appendChild(span)
-       
-    }
-
+        listContainer.appendChild(li)
+    
+        var span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+        
+     }
     inputData.value = ""
     storeData()
-    
 }
 
-listContainer.addEventListener ("click",function(e){
-    if (e.target.tagName == "LI"){
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName == "LI"){
         e.target.classList.toggle("checked")
         storeData()
     }
     else if(e.target.tagName == "SPAN"){
         e.target.parentElement.remove()
-        storeData();
-       
+        storeData()
     }
-}, false)
+},false)
+
 
 function storeData(){
     localStorage.setItem("Data", listContainer.innerHTML)
 }
 
 function showData(){
-    listContainer.innerHTML = localStorage.getItem("Data")
-   
+ listContainer.innerHTML = localStorage.getItem("Data")
 }
+showData()
 
-showData() 
+
